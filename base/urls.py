5f2434e521f15860446b061cd64bd0b15c8febaa
durjoyks <jucse28.376@gmail.com>
2024-02-08 00:21:22 +0600

    commit

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+
+
+
+   path('',views.common_page,name="common_page"),
+   path('login/',views.login_admin,name="login"),
+   path('register/',views.register,name="register"),
+]
---
+++ base/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth import authenticate,login,logout
+
+
+
+def common_page(request):
+
+    return render(request,'base/common_page.html')
+
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form= CreateUserForm()
+ 
+        if request.method=='POST':
+                form=CreateUserForm(request.POST)
+                if form.is_valid():
+                    user=form.save()
+                    username=form.cleaned_data.get('username')
+                    messages.success(request,'Account Created ' +username)
+                    return redirect('login')
+    
+
+        context={'form':form}
+        return render(request,'base/register.html',context)
+
+
+
+
+
+def login_admin(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    else:
+
+       if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)  
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+
+    return render(request, 'base/login.html')
