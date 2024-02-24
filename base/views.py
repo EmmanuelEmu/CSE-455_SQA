ed939df9a6083f3f4001f83ac429eca72d4c6c86
Ashraful Islam <jucse28.373@gmail.com>
2024-02-24 15:38:04 +0600

    The update_department has been done, TTD test has been done and documentation hass been done

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import *
 from .forms import *
@@ -6,27 +6,29 @@ from .filters import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth import authenticate,login,logout
-
-
-
+from django.contrib.auth import authenticate, login, logout
 
 """
+.. module:: base.views
+   :synopsis: Views for the base app.
+
 create_department
 -----------------
 
-View function to create a new department.
+.. function:: create_department(request)
 
-This function handles both GET and POST requests. For GET requests, it renders a form to create a new department. 
-For POST requests, it validates the form data and saves the new department if the form is valid.
+   View function to create a new department.
 
-Parameters:
-    - ``request`` (HttpRequest): The HTTP request object.
+   This function handles both GET and POST requests. For GET requests,
+   it renders a form to create a new department. For POST requests,
+   it validates the form data and saves the new department if the form
+   is valid.
 
-Returns:
-    - HttpResponse: A response to the request.
-
-"""
+   :param request: The HTTP request object.
+   :type request: HttpRequest
+   :return: A response to the request.
+   :rtype: HttpResponse
+   """
 
 def create_department(request):
     """
@@ -37,53 +39,35 @@ def create_department(request):
     it validates the form data and saves the new department if the form
     is valid.
 
-    Parameters:
-    - request (HttpRequest): The HTTP request object.
-
-    Returns:
-    - HttpResponse: A response to the request. 
+    :param request: The HTTP request object.
+    :type request: HttpRequest
+    :return: A response to the request. 
     """
-   # Check if the request method is POST (form submission)
     if request.method == 'POST':
-        # If it's a POST request, create a form instance with the POST data
         form = DepartmentForm(request.POST)
-        # Check if the form data is valid
         if form.is_valid():
-            # If the form is valid, save the form data
             form.save()
-            # Redirect to a success page or view
             return redirect('create_department')
     else:
-        # If it's not a POST request, create an empty form instance
         form = DepartmentForm()
-    
-    # Render the template with the form (either with POST data or empty)
     return render(request, 'base/create_department.html', {'form': form})
 
-
-
 """
+
 department_info
-===============
+---------------
 
-View function to retrieve and display department information.
+.. function:: department_info(request, pk)
 
-Parameters
-----------
-request : HttpRequest
-    HttpRequest object representing the request.
-pk : int
-    Primary key of the department to retrieve.
+   View function to retrieve and display department information.
 
-Returns
--------
-HttpResponse
-    Rendered HTML response displaying department information.
-
-Raises
-------
-Department.DoesNotExist
-    If the department with the given primary key does not exist.
+   :param request: An object representing the request.
+   :type request: HttpRequest
+   :param pk: The primary key of the department to retrieve.
+   :type pk: int
+   :return: A rendered HTML response displaying department information.
+   :rtype: HttpResponse
+   :raises Department.DoesNotExist: If the department with the given primary key does not exist.
 
 """
 
@@ -101,87 +85,53 @@ def department_info(request, pk):
     Raises:
         Department.DoesNotExist: If the department with the given primary key does not exist.
     """
-    # Retrieve the department object with the given primary key
     dept = Department.objects.get(id=pk)
-    
-    # Prepare the context dictionary with the department object
     context = {'dept': dept}
-    
-    # Render the template with the department information
     return render(request, 'base/department_info.html', context)
 
 
-
-
 """
 
-Update Department
+update_department
 -----------------
 
-View function to update a department.
+.. function:: update_department(request, pk)
 
-Args:
-    request (HttpRequest): The HTTP request object.
-    pk (int): The primary key of the department to be updated.
+   View function to update a department.
 
-Returns:
-    HttpResponse: The HTTP response object rendering the template.
+   :param request: An object representing the request.
+   :type request: HttpRequest
+   :param pk: The primary key of the department to update.
+   :type pk: int
+   :return: A rendered HTML response displaying the form to update the department.
+   :rtype: HttpResponse
 
-Details:
-    This function retrieves a department object from the database based on the given primary key (`pk`).
-    It then populates a form with the retrieved department data for updating.
-    If the request method is POST, it validates the form data and saves the updated department if the form is valid.
-    If the rank of the department is changed and a department with the new rank already exists, it displays a message.
-    Finally, it renders the 'base/create_department.html' template with the form data.
-
-Example::
-
-    # Example usage:
-    # To update a department with ID 1
-    update_department(request, 1)
 """
-
-
 
 def update_department(request, pk):
     """
     View function to update a department.
 
     Args:
-        request (HttpRequest): The HTTP request object.
-        pk (int): The primary key of the department to be updated.
+        request: An object representing the request.
+        pk: The primary key of the department to update.
 
     Returns:
-        HttpResponse: The HTTP response object rendering the template.
-
+        HttpResponse: A rendered HTML response displaying the form to update the department.
     """
-    # Retrieve the department object to be updated
     item = Department.objects.get(id=pk)
-    
-    # Create a form instance with the retrieved department data
     form = DepartmentForm(instance=item)
-    
-    # Get the current rank of the department
     current_rank = item.rank
 
     if request.method == 'POST':
-        # If the form is submitted, bind the data to the form
         form = DepartmentForm(request.POST, instance=item)
         if form.is_valid():
-            # If the form data is valid, retrieve the rank from the form
             rank = form.cleaned_data['rank']
-            
-            # Check if the rank has been changed and if a department with the new rank already exists
             if rank != current_rank and Department.objects.filter(rank=rank).exists():
-                # If a department with the new rank already exists, display a message
-                messages.info(request, 'Already exists a department with this rank')
+                messages.info(request, 'Already exists a dept with this Rank')
             else:
-                # If the rank is valid and unique, save the form data
                 form.save()
-                # Redirect to a success page or view
                 return redirect('create_department')
 
-    # Render the template with the form
     context = {'form': form}
     return render(request, 'base/create_department.html', context)
-
